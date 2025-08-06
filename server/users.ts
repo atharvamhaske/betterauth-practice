053d2645eaa20ee74d531079347168dc99269119
atharvamhaske <atharvamhaske76@gmail.com>
2025-08-06 23:59:37 +0530

    feat:added shadcn components

--- server/users.ts
+++ server/users.ts
@@ -1,3 +1,23 @@
 "use server"
 
-export const signUp
+import { auth } from "@/app/lib/auth"
+import { APIError } from "better-auth";
+
+export const signUp = async (formData: FormData) => {
+    try {
+        await auth.api.signUpEmail({
+            body: {
+                name: "",
+                email: "",
+                password: ""
+            }
+        });
+        return { success: true };
+        
+    } catch (error) {
+        if(error instanceof APIError) {
+            console.log("API Error:", error.message)
+        }
+        
+    }
+}
